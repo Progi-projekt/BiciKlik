02ec4c18bc7dd5cdd305e5b2088b6e372b268b88
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-02 18:04:09 +0100

    Navbar layout and appearance
    The navbar is in"./components/heading.tsx" used basic HTML and CSS, added it to the "App.tsx" and did some minimal appearence changes on "App.css".

---
+++ IzvorniKod/frontend/src/components/heading.tsx
@@ -0,0 +1,20 @@
+import logo from 'Bicklik.png'
+import React from 'react';
+import "./heading.css"
+
+const Heading =() => {
+    return (
+      <div className="Heading">
+        <img src={logo} alt='logo' className='logo'></img>
+
+        <ul>
+          <li>News Feed</li>
+          <li>Feature1</li>
+          <li>Feature2</li>
+        </ul>
+        
+        <button className='LogIn'>Log In</button>
+      </div>
+    );
+  }
+  export default Heading;
