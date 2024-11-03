6eec409d2ba415fa1e64d6bf34eaf2d8f05a2def
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-03 15:23:02 +0100

    Footer appearance
    Added a footer "Footer.tsx" and it's CSS file. It might need future styling to appear at the bottom and for the buttons to work.

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Heading from 'components/heading';
+import Footer from 'components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Heading></Heading>
+      <Heading/>
+      <Footer/>
     </div>
   );
 }
