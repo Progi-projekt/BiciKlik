8d2432e4ab82982e3b71d06cc6fc8f87d480195f
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-03 21:16:31 +0100

    NewsFeed appearence and layout
    Added 'NewsFeed.tsx' and CSS file and made it look like the design. It's non functional as of yet.

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Heading from 'components/heading';
 import Footer from 'components/footer';
+import NewsFeed from 'components/NewsFeed';
 
 function App() {
   return (
     <div className="App">
       <Heading/>
+      <NewsFeed/>
       <Footer/>
     </div>
   );
