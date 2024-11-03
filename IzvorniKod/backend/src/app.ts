1b14c9d2c304ce93c1bad5eeb702986bebad8ce3
Marin Prusac <prusac.marin@gmail.com>
2024-11-03 22:37:50 +0100

    changes to config and main files necessery for running the database

--- IzvorniKod/backend/src/app.ts
+++ IzvorniKod/backend/src/app.ts
@@ -1,12 +1,20 @@
 // src/app.ts
 import express, { Application, Request, Response, NextFunction } from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
+import sequelize from './config/database';
 
 const app: Application = express();
+var db_connected: boolean = false;
+
+
+sequelize.authenticate()
+.then(() => {
+    console.log('Connected to the database');
+    db_connected = true;
+})
+.catch((err: any) => {
+    console.error('Unable to connect to the database:', err);
+});
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -17,6 +25,15 @@ app.use(express.static(path.join(__dirname, '../../frontend/build')));
 // API route example
 app.get('/api/health', (req, res) => {
     res.json({ status: 'OK' });
+});
+
+// DB health check
+app.get('/db/health', (req, res) => {
+    if (db_connected) {
+        res.json({ status: 'OK' });
+    } else {
+        res.json({ status: 'ERROR' });
+    }
 });
 
 // Serve React frontend (if integrated)
