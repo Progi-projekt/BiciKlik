ee9a1889f56d4259bc439a5eac8b4d35e13094dc
Oliver Kreitmeyer <oliverkreitmeyer@gmail.com>
2024-11-04 21:31:48 +0100

    made a simple username nad passowrd form for logging in.

--- IzvorniKod/frontend/src/components/LoginSignUp.tsx
+++ IzvorniKod/frontend/src/components/LoginSignUp.tsx
@@ -3,9 +3,22 @@ import "./loginsignup.css";
 
 const loginsignup =() => {
     return (
-      <div className="loginsignup">
-        <h1>Wow!</h1>
-      </div>
+    <div className="loginsignup">
+        <form action="" method="post">
+
+            <div className="container">
+                <label htmlFor="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required />
+
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required />
+
+                <button type="submit">Login</button>
+            </div>
+        </form>
+
+    </div>
     );
   }
-  export default loginsignup;
+
+export default loginsignup;
