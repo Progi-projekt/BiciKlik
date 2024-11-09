af18f748a42126274b246514d12af8b83dc7f8ef
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-09 17:08:44 +0100

    Fixed an import
    Fixed an import so it can work on the server.

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Heading from './components/heading';
 import Footer from './components/footer';
 import NewsFeed from './pages/NewsFeed';
-import Loginsignup from 'pages/LoginSignUp';
+import Loginsignup from './pages/LoginSignUp';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
