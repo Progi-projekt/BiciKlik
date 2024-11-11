11f48994486ad5986c8c2c411c91c29607c39bd6
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-11 23:37:46 +0100

    Fixed img

--- IzvorniKod/frontend/src/pages/NewsFeed.tsx
+++ IzvorniKod/frontend/src/pages/NewsFeed.tsx
@@ -43,7 +43,7 @@ const Newsfeed =() => {
               <p className='nazivRute'>{event.event_name}</p>
               <p className='vrijemeDatum'>{event.event_time}</p>
               <p>{event.description}</p>
-              <img src={'https://localhost:3000/images/${event.route_id}.png'} alt='RouteImg' className='slikarute' />
+              <img src={`https://biciklik.duckdns.org/images/${event.route_id}.png}`} alt='RouteImg' className='slikarute' />
             </div>
           </div>
         ))}
