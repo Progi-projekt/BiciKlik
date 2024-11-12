6248c24d0bd893a7a7ad4816e1006fcba1191674
Andrej Filipčić <87577406+Andrej-F@users.noreply.github.com>
2024-11-12 10:39:56 +0100

    Revert "Chat (mock)"

--- IzvorniKod/frontend/src/components/heading.tsx
+++ IzvorniKod/frontend/src/components/heading.tsx
@@ -10,9 +10,8 @@ const Heading =() => {
 
         <ul>
           <li><Link to={"/"} style={{textDecoration: 'none', color: 'inherit'}}>News Feed</Link></li>
-
-          <li><Link to={"/chat"} style={{textDecoration: 'none', color: 'inherit'}}>Chat</Link></li>
-          <li>Feature2</li>
+          <li className='Feature1'>Feature1</li>
+          <li className='Feature2'>Feature2</li>
         </ul>
         
         <Link to={"/login"}><button className='LogIn'>Log In</button></Link>
