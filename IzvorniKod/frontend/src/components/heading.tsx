f8d316df762d4247866ae0211954a31503e3eddb
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-11-12 13:46:36 +0100

    Logged in Icon - Trying to read from cookies

--- IzvorniKod/frontend/src/components/heading.tsx
+++ IzvorniKod/frontend/src/components/heading.tsx
@@ -1,9 +1,25 @@
 import logo from '../assets/Bicklik.png'
-import React from 'react';
 import "./heading.css"
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import icon from '../assets/icon.png';
 
 const Heading =() => {
+    const [LoggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+      const connectSid = Cookies.get('connect.sid');
+      const gState = Cookies.get('g:state');
+      const loggedInAs = Cookies.get('loggedInAs');
+  
+      if (connectSid && gState && loggedInAs) {
+        setLoggedIn(true);  
+      } else {
+        setLoggedIn(false); 
+      }
+    }, []);
+
     return (
       <div className="Heading">
         <img src={logo} alt='logo' className='logo'></img>
@@ -13,8 +29,11 @@ const Heading =() => {
           <li className='Feature1'>Feature1</li>
           <li className='Feature2'>Feature2</li>
         </ul>
-        
-        <Link to={"/login"}><button className='LogIn'>Log In</button></Link>
+        {LoggedIn ? (
+            <img src={icon} alt='ikona' className='ikona'/>
+        ) : (
+          <Link to={"/login"}> <button className='LogIn'>Log In</button> </Link>
+        )}
       </div>
     );
   }
