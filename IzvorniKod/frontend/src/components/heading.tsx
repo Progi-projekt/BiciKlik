d10e518eca539489dd34e3569f420849e88ddd73
Andrej Filipčić <af55048@fer.hr>
2025-01-02 20:30:06 +0100

    Added logout to frontend

--- IzvorniKod/frontend/src/components/heading.tsx
+++ IzvorniKod/frontend/src/components/heading.tsx
@@ -1,14 +1,14 @@
 import logo from '../assets/Bicklik.png'
 import "./heading.css"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import icon from '../assets/icon.png';
-
 
 const Heading = () => {
   const [LoggedIn, setLoggedIn] = useState(false);
   const [userRole, setUserRole] = useState(null); //spremi user role
   const [openProfile, setOpenProfile] = useState(false); //za pop up za logOut
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("/auth/getAuthorization", { method: "GET", credentials: "include"})
@@ -31,32 +31,49 @@ const Heading = () => {
     });
   }, []);
 
-    return (
-      <div className="Heading">
-        <img src={logo} alt='logo' className='logo'></img>
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+      if (response.ok) {
+        setLoggedIn(false);
+        navigate('/login');
+      } else {
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-        <ul>
-          <li><Link to={"/"} className="clickable">News Feed</Link></li>
-          <li><Link to={"/chat"} className="clickable">Chat</Link></li>
-          {LoggedIn && <li><Link to={"/createRoute"} className="createButton">Create Route</Link></li>} 
-        </ul>
+  return (
+    <div className="Heading">
+      <img src={logo} alt='logo' className='logo'></img>
 
-        {LoggedIn ? (
-          <>
-            <img src={icon} alt='ikona' className='ikona' onClick={() => setOpenProfile((prev) => !prev)}/>
-            {openProfile &&
-            <div className='dropDown-container'>
-            <ul className='dropDown'>
-            <li className='logoutItem'>LogOut</li>
-            </ul>
-            </div>
-            }
-          </>
-        ) : (
-          <Link to={"/login"}> <button className='LogIn'>Log In</button> </Link>
-        )}
+      <ul>
+        <li><Link to={"/"} className="clickable">News Feed</Link></li>
+        <li><Link to={"/chat"} className="clickable">Chat</Link></li>
+        {LoggedIn && <li><Link to={"/createRoute"} className="createButton">Create Route</Link></li>} 
+      </ul>
 
-      </div>
-    );
-  }
-  export default Heading;
+      {LoggedIn ? (
+        <>
+          <img src={icon} alt='ikona' className='ikona' onClick={() => setOpenProfile((prev) => !prev)}/>
+          {openProfile &&
+          <div className='dropDown-container'>
+          <ul className='dropDown'>
+          <li className='logoutItem' onClick={handleLogout}>LogOut</li>
+          </ul>
+          </div>
+          }
+        </>
+      ) : (
+        <Link to={"/login"}> <button className='LogIn'>Log In</button> </Link>
+      )}
+
+    </div>
+  );
+}
+export default Heading;
