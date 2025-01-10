49bd48640481f246ee6750bbae451d5dc17ed395
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2025-01-10 11:09:28 +0100

    Styled the 404 page
    Did the .css for the 404 page.

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -10,6 +10,7 @@ import CreateRoute from './pages/CreateRoute';
 import NotFound from './pages/NotFound';
 
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Newsfeed from './pages/NewsFeed';
 
 function App() { 
   return (
@@ -17,12 +18,12 @@ function App() {
     <div className="App">
       <Heading/>
       <Routes>      
-      <Route path='/' element={<NewsFeed/>}/>
+      <Route path='/' element={<Newsfeed/>}/>
       <Route path='/login' element={<Loginsignup/>}/>
       <Route path='/chat' element={<Chat/>}/>
       <Route path='/event/:event_id' element={<ClickedEvent/>}/>  
       <Route path='/createRoute' element={<CreateRoute/>}/>
-      <Route path='*' element={<NotFound/>}/> {/* Catch-all route for 404 */}
+      <Route path='*' element={<NotFound/>}/>  {/*Catch-all route for 404 */}
       </Routes>
       <div className="footer">
       <Footer></Footer>
--- IzvorniKod/frontend/src/pages/NotFound.tsx
+++ IzvorniKod/frontend/src/pages/NotFound.tsx
@@ -1,10 +1,15 @@
 // frontend/src/pages/NotFound.tsx
 import React from 'react';
+import '../components/notfound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div className='notfound-container'>
+      <p className='notfound-text'>404</p>
+      <p className='notfound-text2'>Page Not Found</p>
+      <p className='notfound-text3'>The page you requested was not found...</p>
+      <Link to={"/"}> <button className='button-notfound'>Home</button></Link>
     </div>
   );
 };
