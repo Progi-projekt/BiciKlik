b25a87cee569b29542a6e6c53ac64816ee075034
Moodax <64415905+Moodax@users.noreply.github.com>
2025-01-13 14:47:25 +0100

    Heading update

--- IzvorniKod/frontend/src/components/heading.tsx
+++ IzvorniKod/frontend/src/components/heading.tsx
@@ -1,22 +1,44 @@
-import logo from '../assets/Bicklik.png';
-import "./heading.css";
+import logo from '../assets/Bicklik.png'
+import "./heading.css"
 import { Link, useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import icon from '../assets/icon.png';
-import { useAuth } from '../AuthContext';
 
 const Heading = () => {
-  const { loggedIn, userRole, checkAuthStatus } = useAuth();
-  const [openProfile, setOpenProfile] = useState(false);
+  const [LoggedIn, setLoggedIn] = useState(false);
+  const [userRole, setUserRole] = useState(null); //spremi user role
+  const [openProfile, setOpenProfile] = useState(false); //za pop up za logOut
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("/auth/getAuthorization", { method: "GET", credentials: "include"})
+    .then((response) => {
+      if (!response.ok){
+        throw new Error("Failed fetch");}
+        return response.json();
+      })
+    .then((data) => {
+      if (data.loggedInAs) {
+        setLoggedIn(true);
+        setUserRole(data.role); 
+      } else {
+        setLoggedIn(false); //true/false za hardkodiranje
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      setLoggedIn(false);
+    });
+  }, []);
+
   const handleLogout = async () => {
     try {
-      const response = await fetch('/api/auth/logout', {
+      const response = await fetch('/auth/logout', {
         method: 'POST',
         credentials: 'include',
       });
       if (response.ok) {
-        checkAuthStatus();
+        setLoggedIn(false);
         navigate('/login');
       } else {
         console.error('Logout failed');
@@ -29,35 +51,37 @@ const Heading = () => {
   return (
     <div className="Heading">
       <img src={logo} alt='logo' className='logo'></img>
+
       <ul>
         <li><Link to={"/"} className="clickable">News Feed</Link></li>
-        {loggedIn && <li>
+        {LoggedIn && <li>
                 <Link 
                     to="/chat" 
-                    state={{}}
+                    state={{ email, name }}
                     className="clickable"
                 >
                     Chat
                 </Link>
             </li>} 
-        {loggedIn && <li><Link to={"/createRoute"} className="createButton">Create Route</Link></li>} 
+        {LoggedIn && <li><Link to={"/createRoute"} className="createButton">Create Route</Link></li>} 
       </ul>
-      {loggedIn ? (
+
+      {LoggedIn ? (
         <>
-          <img src={icon} alt='ikona' className='ikona' onClick={() => setOpenProfile((prev) => !prev)} />
+          <img src={icon} alt='ikona' className='ikona' onClick={() => setOpenProfile((prev) => !prev)}/>
           {openProfile &&
-            <div className='dropDown-container'>
-              <ul className='dropDown'>
-                <li className='logoutItem' onClick={handleLogout}>LogOut</li>
-              </ul>
-            </div>
+          <div className='dropDown-container'>
+          <ul className='dropDown'>
+          <li className='logoutItem' onClick={handleLogout}>LogOut</li>
+          </ul>
+          </div>
           }
         </>
       ) : (
         <Link to={"/login"}> <button className='LogIn'>Log In</button> </Link>
       )}
+
     </div>
   );
 }
-
 export default Heading;
