97945f8222be5fdc8efe05f44323ed8b162c5883
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2025-01-13 17:01:57 +0100

    Simple createEvent form outline
    Added an outline for the createEvent

--- IzvorniKod/frontend/src/pages/CreateEvent.tsx
+++ IzvorniKod/frontend/src/pages/CreateEvent.tsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 import '../components/createevent.css';
 
-function CreateEvent () {
+const CreateEvent = () => {
+    const [eventName, setEventName] = useState('');
+    const [shortDescription, setShortDescription] = useState('');
+    const [eventTime, setEventTime] = useState('');
+    const [description, setDescription] = useState('');
+    const [routeId, setRouteId] = useState('');
+
+    const handleEventSubmit = async (e: React.FormEvent) => {
+
+    }
+
     return (
-        <div>Hello</div>
+        <div className='eventcreation-container'>
+            <h2>Create Event</h2>
+            <form onSubmit={handleEventSubmit}>
+                <div>
+                    <label>Event name:</label>
+                    <input type="text" 
+                           name="eventName" 
+                           value={eventName}
+                           onChange={(e) => setEventName(e.target.value)}
+                           required>
+                    </input>
+                </div>
+                <div>
+                    <label>Event time:</label>
+                    <input type="datetime-local"
+                           name="eventTime"
+                           value={eventTime}
+                           onChange={(e) => setEventTime(e.target.value)}
+                           required>
+                    </input>
+                </div>
+                <div>
+                    <label>Route id:</label>
+                    <input type="text"
+                           name="routeId"
+                           value={routeId}
+                           onChange={(e) => setRouteId(e.target.value)}
+                           required>
+                    </input>
+                </div>
+                <div>
+                    <label>Short Description:</label>
+                    <textarea
+                           name="shortDescription"
+                           value={shortDescription}
+                           onChange={(e) => setShortDescription(e.target.value)}
+                           required>
+                    </textarea>
+                </div>
+                <div>
+                    <label>Description:</label>
+                    <textarea
+                           name="description"
+                           value={description}
+                           onChange={(e) => setDescription(e.target.value)}
+                           required>
+                    </textarea>
+                </div>
+                <button type="submit">Create Event</button>
+            </form>
+        </div>
     );
 }
 
