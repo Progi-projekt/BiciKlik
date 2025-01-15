7ddc4dbee455054e8377aa2ee6bbf63aafd90894
Oliver Kreitmeyer <oliverkreitmeyer@gmail.com>
2025-01-15 23:17:18 +0100

    connected FE and BE in myRoutes

--- IzvorniKod/frontend/src/pages/Routes.tsx
+++ IzvorniKod/frontend/src/pages/Routes.tsx
@@ -1,9 +1,28 @@
 import { Link } from 'react-router-dom';
 import '../components/routes.css';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const Routess = () => {
+    const [savedRoutes, setSavedRoutes] = useState<string[]>([]);
+    const [ownedRoutes, setOwnedRoutes] = useState<string[]>([]);
+
+    const fetchRoutes = async () => {
+        try {
+            const response = await fetch('/api/route/myRoutes');
+            const data = await response.json();
+
+            setSavedRoutes(data.savedRoutes);
+            setOwnedRoutes(data.ownedRoutes);
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching routes:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchRoutes();
+    }, []);
+
   return (
     <div className='routes-container'>
         <div className='routes-container2'>
