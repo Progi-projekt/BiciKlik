7c8f7b2aa89ffba322a2e70e3791d729ba807cb6
Marin Prusac <prusac.marin@gmail.com>
2025-01-19 03:12:59 +0100

    admin functionalities, routed

--- IzvorniKod/backend/src/controllers/admin.controller.ts
+++ IzvorniKod/backend/src/controllers/admin.controller.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express';
 import { AdminService } from '../services/admin.service';
 import { EventService } from '../services/event.service';
+import { RouteService } from '../services/route.service';
 
 export class AdminController {
     private adminService: AdminService;
+	private routeService: RouteService;
+	private eventService: EventService;
 
     constructor() {
         this.adminService = new AdminService();
+		this.routeService = new RouteService();
+		this.eventService = new EventService();
     }
 
 	public isAdmin = async (email: string ) => {
@@ -54,6 +59,73 @@ export class AdminController {
             res.status(200).json("done");
         } catch (error) {
             console.error('Error fetching user:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    };
+
+	public removeEntry = async (req: Request, res: Response) => {
+        const email = req.cookies.loggedInAs; // Ensure this cookie is set
+		const eventId = req.params.eventId;
+		const user = req.params.user;
+        if (!email) {
+            return res.status(401).json({ error: 'User not authenticated' });
+        }
+		if(!await this.isAdmin(email)){
+			return res.status(401).json({ error: 'User not authorized' });
+		}
+    
+        try {
+            const success = await this.eventService.removeResult(eventId, user);
+            if (!success) {
+                return res.status(404);
+            }
+            res.status(200).json("done");
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    };
+
+	public deleteEvent = async (req: Request, res: Response) => {
+        const email = req.cookies.loggedInAs; // Ensure this cookie is set
+		const eventId = req.params.eventId;
+        if (!email) {
+            return res.status(401).json({ error: 'User not authenticated' });
+        }
+		if(!await this.isAdmin(email)){
+			return res.status(401).json({ error: 'User not authorized' });
+		}
+    
+        try {
+            const success = await this.eventService.eraseEvent(eventId);
+            if (!success) {
+                return res.status(404);
+            }
+            res.status(200).json("done");
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    };
+
+	public deleteRoute = async (req: Request, res: Response) => {
+        const email = req.cookies.loggedInAs; // Ensure this cookie is set
+		const routeId = req.params.routeId;
+        if (!email) {
+            return res.status(401).json({ error: 'User not authenticated' });
+        }
+		if(!await this.isAdmin(email)){
+			return res.status(401).json({ error: 'User not authorized' });
+		}
+    
+        try {
+            const success = await this.routeService.deleteRoute(routeId);
+            if (!success) {
+                return res.status(404);
+            }
+            res.status(200).json("done");
+        } catch (error) {
+            console.error('Error:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     };
