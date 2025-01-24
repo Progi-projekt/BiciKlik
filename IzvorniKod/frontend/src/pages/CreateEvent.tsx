073a5c2cbac721aa132da2dd838949681d156f57
Oliver Kreitmeyer <oliverkreitmeyer@gmail.com>
2025-01-24 16:16:48 +0100

    made a custom dropdown for selecting the name and seeing the image of the route in createevents. might need a bit more polish.

--- IzvorniKod/frontend/src/pages/CreateEvent.tsx
+++ IzvorniKod/frontend/src/pages/CreateEvent.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-
+import CustomDropdown from '../components/CustomDropdown';
 import '../components/createevent.css';
 
 const CreateEvent = () => {
@@ -9,11 +9,28 @@ const CreateEvent = () => {
     const [eventTime, setEventTime] = useState('');
     const [description, setDescription] = useState('');
     const [routeId, setRouteId] = useState('');
+    const [routes, setRoutes] = useState([]);
+    const [hoveredRoute, setHoveredRoute] = useState(null);
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchRoutes = async () => {
+            try {
+                const response = await fetch('/api/route/myRoutes');
+                const data = await response.json();
+                const combinedRoutes = [...data.ownedRoutes, ...data.savedRoutes];
+                setRoutes(combinedRoutes);
+            } catch (error) {
+                console.error('Error fetching my routes:', error);
+            }
+        };
+        fetchRoutes();
+    }, []);
+
     const handleEventSubmit = async (e: React.FormEvent) => {
-        e.preventDefault(); //zaustavja reload stranice
+        e.preventDefault();
 
         try {
             const response = await fetch('/api/event/createEvent', {
@@ -31,10 +48,11 @@ const CreateEvent = () => {
                 setEventTime('');
                 setDescription('');
                 setRouteId('');
+                setRoutes([]);
 
                 setTimeout(() => {
                     navigate('/');
-                }, 1000);
+                }, 500);
             } else {
                 alert('Failed to create event');
             }
@@ -43,64 +61,88 @@ const CreateEvent = () => {
         }
     };
 
+    const handleMouseEnter = (routeId) => {
+        setHoveredRoute(routeId);
+    };
 
+    const handleMouseLeave = () => {
+        setHoveredRoute(null);
+    };
+
+    const handleMouseMove = (e) => {
+        setMousePosition({ x: e.clientX, y: e.clientY });
+    };
 
     return (
-        <div className="eventyay">
-        <div className='eventcreation-container'>
-            <h2 className="naslov">Create Event</h2>
-            <form onSubmit={handleEventSubmit}>
-                <div className="eventName">
-                    <label>Event name:</label>
-                    <input 
-                        type="text" 
-                        name="eventName" 
-                        value={eventName}
-                        onChange={(e) => setEventName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="eventTime">
-                    <label>Event time:</label>
-                    <input 
-                        type="datetime-local"
-                        name="eventTime"
-                        value={eventTime}
-                        onChange={(e) => setEventTime(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="eventRoute">
-                    <label>Route id:</label>
-                    <input 
-                        type="text"
-                        name="routeId"
-                        value={routeId}
-                        onChange={(e) => setRouteId(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="shortDescription">
-                    <label>Short Description:</label>
-                    <textarea
-                        name="shortDescription"
-                        value={shortDescription}
-                        onChange={(e) => setShortDescription(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="description">
-                    <label>Description:</label>
-                    <textarea
-                        name="description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit" className="submitform">Create Event</button>
-            </form>
-        </div>
+        <div className="eventyay" onMouseMove={handleMouseMove}>
+            <div className='eventcreation-container'>
+                <h2 className="naslov">Create Event</h2>
+                <form onSubmit={handleEventSubmit}>
+                    <div className="eventName">
+                        <label>Event name:</label>
+                        <input 
+                            type="text" 
+                            name="eventName" 
+                            value={eventName}
+                            onChange={(e) => setEventName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="eventTime">
+                        <label>Event time:</label>
+                        <input 
+                            type="datetime-local"
+                            name="eventTime"
+                            value={eventTime}
+                            onChange={(e) => setEventTime(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="eventRoute">
+                        <label>Route:</label>
+                        <CustomDropdown 
+                            options={routes.map(route => ({ value: route.route_id, label: route.route_name }))}
+                            value={routeId}
+                            onChange={setRouteId}
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                        {hoveredRoute && (
+                            <div 
+                                className="route-image-popup" 
+                                style={{ 
+                                    left: mousePosition.x + 20, 
+                                    top: mousePosition.y - 305 // Adjust this value based on the image height
+                                }}
+                            >
+                                <img src={`/images/route-${hoveredRoute}.png`} alt="Route" />
+                            </div>
+                        )}
+                    </div>
+                    <div className="shortDescription">
+                        <label>Short Description:</label>
+                        <textarea
+                            name="shortDescription"
+                            value={shortDescription}
+                            onChange={(e) => setShortDescription(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="description">
+                        <label>Description:</label>
+                        <textarea
+                            name="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="submitform">Create Event</button>
+                </form>
+            </div>
+            <div>
+                Mouse Position: {mousePosition.x}, {mousePosition.y}
+            </div>
         </div>
     );
 };
